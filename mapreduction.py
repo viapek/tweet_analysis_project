20e00435540f6e8013d58653d39c6c4388ac8aa3
Jeremy Watt <jwatt37@hotmail.com>
2015-10-01 13:37:59 +0900

    Fully functioning rolling collection. Export, drop, zip. Unzip, import, zip. Mapreduce for word counts at for 5 minute window.

--- mapreduction.py
+++ mapreduction.py
@@ -15,53 +15,48 @@ db = client[config.s_WorkingDatabase]
 map = Code("function map() {"
            "   var littlewords = ['ALL', 'NOT', 'NO', 'YOURS', 'OR', 'SO', 'FOR', 'ME', 'HE', 'HIM', 'HIS', 'IN', 'IF', 'WITH', 'OF', 'WHAT', 'WHO', 'WHY', 'HOW', 'WHEN', 'WHERE', 'AS', 'AN', 'AT', 'THE', 'IT', 'FROM', 'TO', 'BY', 'BUT', 'ONLY', 'YOU', 'YOUR', 'WE', 'WILL', 'WOULD', 'THAT', 'THEY', 'THIS', 'ON', 'MY'];"
            "   var littleverbs = ['DO', 'BE', 'IS', 'WAS', 'ARE', 'WERE', 'HAS', 'HAD', 'HAVE'];"
+           "   var t_time = parseInt(this.timestamp_ms);"
+           "   t_time = (t_time+150000)/300000;"
+           "   t_time = parseInt(t_time)*300;"
            "   var cnt = this.text;"
-           "   var words = cnt.match(/\w+/g);"
-           "   if (words == null) {"
+           "   if (!cnt || isNaN(t_time)) {"
            "        return;"
            "   }"
-           "   for (var i = 0; i < words.length; i++) {"
-           "        if (words[i].length > 1) {"
-           "            if ((littlewords.indexOf(words[i].toUpperCase()) ==  -1) && (littleverbs.indexOf(words[i].toUpperCase()) ==  -1)) {"
-           "                emit({word:words[i].toLowerCase() }, {count:1 });"
-           "            }"
-           "        }"
+           "   var words = cnt.match(/\w+/g);"
+           "   if (words == null) {"
+           "      return;"
            "   }"
+           "   var elements = {};"
+           "   for (var i = 0; i < words.length; i++) {"
+           "     if (words[i].toUpperCase() in littlewords || words[i].toUpperCase() in littleverbs) {"
+           "     } else {"
+           "        if (words[i].toLowerCase() in elements) {"
+           "            elements[words[i].toLowerCase()]++;"
+           "        } else {"
+           "            elements[words[i].toLowerCase()] = 1;"
+           "        }"
+           "      }"
+           "    }"
+           "    emit({ 'id': t_time }, {'words': elements});"
            "}")
 
-reduce = Code("function reduce(key, counts) {"
-            "   var cnt = 0;"
-            "   for (var i = 0; i < counts.length; i++) {"
-            "      cnt = cnt + counts[i].count;"
-            "   }"
-            "   return { count:cnt };"
+
+reduce = Code("function reduce(key, values) { "
+            "    var elements = {};"
+            "    for (var i = 0; i < values.length; i++) {"
+            "        for (var words in values[i]) {"
+            "            for (var word in values[i].words){ "
+            "                if (word in elements){"
+            "                    elements[word] = elements[word] + values[i].words[word];"
+            "                } else {"
+            "                    elements[word] = values[i].words[word];"
+            "                }"
+            "            }"
+            "        }"
+            "    }"
+            "    return elements;"
             "}")
 
 colHandle = db[config.s_WorkingCollection]
 
-result = colHandle.map_reduce(map, reduce, config.s_WorkingResults)
-
-try:
-  db.drop_collection(colHandle.name)
-except (ValueError, KeyError, TypeError) as e:
-  print "Error in dropping working collection:"
-  print e
-else:
-  print "Dropped like it was hot"
-
-colHandle = db[config.s_WorkingResults]
-
-pipe = [{'$match': { 'value.count': { '$gte': 1} }}, { '$sort': { 'value.count': -1}}, {'$limit':50}]
-result = colHandle.aggregate(pipeline=pipe)
-
-s_ForWordCloud = ''
-
-for line in result:
-    print line
-    #s_ForWordCloud += "{0} ".format(line.values()[0]['word'])*int(line.values()[1]['count'])
-"""     
-wordcloud = WordCloud().generate(s_ForWordCloud)
-img = plt.imshow(wordcloud)
-plt.axis("off")
-plt.show()
-"""
+colHandle.map_reduce(map, reduce, config.s_WorkingResults)
