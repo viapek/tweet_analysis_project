028323829ed07429a2551850f491f32cbfdd40a5
Jeremy Watt <jwatt37@hotmail.com>
2015-10-05 13:36:22 +0900

    stash

--- dnskeepalive.py
+++ dnskeepalive.py
@@ -13,12 +13,14 @@ import urllib2 as urllib
 i_DefaultSeconds = 300
 s_TargetWeb = "google.com"
 
+observers = []
+
 def getHTMLTitle(s):
     # take a urllib2.open results and get the contents of <title></title>
     return s[s.find("<title>") + len("<title>"):s.find("</title>")]
 
  
-def sleeper():
+def poller():
     while True:
         # Run our time.sleep() command,
         # and show the before and after time
@@ -34,15 +36,55 @@ def sleeper():
             pass
         else:
             print site,": ",getHTMLTitle(res.read())," retrieved at ", time.ctime().split()[3], " in ", time.clock()-start_time
-    
-# Get user input
-try:
+
+def watchOutForMe(obj, s_methodName):
+  #this registers the object and the method name to call
+  #then check for network performance, mainly to address network resting
+  #if there is a network problem this will call the s_methodName of the object
+  if not obj in observers:
+      if obj.hasattr(s_methodName):
+        obeservers.append([obj, s_methodName])
+      else:
+        raise ValueError
+  #TODO store object in an array with the method name as a tuple
+  
+def endMyWatch(obj):
+  #this deregisters the object
+  #TODO check if the object exists
+  if obj in observers:
+  #TODO remove the object from the dictionary
+    try:
+        observers.remove(obj)
+  #TODO return result
+        pass
+    else:
+        raise ValueError
+
+  
+def callAllWatchers():
+  #TODO go through the registered objects and call their methods
+  for observer, method in observers:
+      s_command = "{0}.{1}()".format(observer, method)
+      print "Executing {0}".format(s_command)
+      objCode = compile(s_command)
+      exec(objCode)
+
+
+def restartNetwork():
+  #TODO shutdown and restart the network service or refresh DHCP information
+  print "Restart Network"
+
+
+if __name__ == '__main__':
+
+  # Get user input
+  try:
     num = input('How long to wait: [{0}] '.format(i_DefaultSeconds))
-except SyntaxError:
+  except SyntaxError:
     num = i_DefaultSeconds
 
-# Try to convert it to a float
-while True:
+  # Try to convert it to a float
+  while True:
     try:
         num = float(num)
     except ValueError:
@@ -51,18 +93,18 @@ while True:
     else:
         break
     
-# get address
-site = raw_input("Please enter a site address to call: [{0}]".format(s_TargetWeb))
-if site == '':
+  # get address
+  site = raw_input("Please enter a site address to call: [{0}]".format(s_TargetWeb))
+  if site == '':
     site = s_TargetWeb
 
-# simple url prep
+  # simple url prep
 
-if not site.startswith('http'):
+  if not site.startswith('http'):
     site = "http://" + site
  
-try:
-    sleeper()
-except KeyboardInterrupt:
+  try:
+    poller()
+  except KeyboardInterrupt:
     print('\n\nKeyboard exception received. Exiting.')
     exit()
